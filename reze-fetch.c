b79d3f971cf78655e6d0b3a0f8e82958a53d0bb5
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-04-09 22:35:22 +0200

    added error handling for first sysctlbyname calls

--- reze-fetch.c
+++ reze-fetch.c
@@ -9,18 +9,20 @@ int main () {
 	printf("\n");
 	size_t len_osversion = sizeof(osversion);
 	size_t len_hostname = sizeof(hostname);
-	sysctlbyname("kern.osrelease", NULL, &len_osversion, NULL, 0);
-	if(sysctlbyname("kern.osrelease", osversion, &len_osversion, NULL, 0) == -1) {
-		perror("sysctl kern.osrelease");
-    		return 1;	
-	}
-	sysctlbyname("kern.hostname", NULL, &len_hostname, NULL, 0);
+	if (sysctlbyname("kern.osrelease", NULL, &len_osversion, NULL, 0) == 0){
 	
-	if(sysctlbyname("kern.hostname", hostname, &len_hostname, NULL, 0) == -1) {
-		perror("sysctl kern.hostname");
-		return 1;
-	}	
+		if(sysctlbyname("kern.osrelease", osversion, &len_osversion, NULL, 0) == -1){ 
+			perror("sysctl kern.osrelease");
+    			return 1;
+		}
+	}else{return 1;}
+	if (sysctlbyname("kern.hostname", NULL, &len_hostname, NULL, 0) == 0){
 
+                if(sysctlbyname("kern.hostname", hostname, &len_hostname, NULL, 0) == -1){
+                        perror("sysctl kern.hostname");
+                        return 1;
+                }
+        }else{return 1;}
 	printf("\tOS: %s\n", osversion);
 	printf("\tHostname: %s\n", hostname);
 	return 0;
