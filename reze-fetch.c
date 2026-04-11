bec565db6537d7a0063698781c0c9fb98712ae09
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-04-12 01:56:25 +0200

    dynamically allocated mem for osversion and hostname

--- reze-fetch.c
+++ reze-fetch.c
@@ -2,29 +2,40 @@
 #include <sys/sysctl.h>
 
 int main () {
-	char osversion[256];	
-	char hostname[256];
 
 	printf(" .✦ ݁˖ REZE--FETCH .✦ ݁˖\n");
 	printf("\n");
-	size_t len_osversion = sizeof(osversion);
-	size_t len_hostname = sizeof(hostname);
-	if (sysctlbyname("kern.osrelease", NULL, &len_osversion, NULL, 0) == 0){
+	size_t len_osversion;
+	size_t len_hostname;
 	
-		if(sysctlbyname("kern.osrelease", osversion, &len_osversion, NULL, 0) == -1){ 
-			perror("sysctl kern.osrelease");
-    			return 1;
-		}
+	char *osversion_ptr;
+	char *hostname_ptr;
+	if (sysctlbyname("kern.osrelease", NULL, &len_osversion, NULL, 0) == 0){
+		
+		osversion_ptr = malloc(len_osversion);
+		if (osversion_ptr != NULL){
+			if (sysctlbyname("kern.osrelease", osversion_ptr, &len_osversion, NULL, 0) == -1){ 
+				perror("sysctl kern.osrelease");
+    				return 1;
+			}
+		}else{return 1;} 
+
 	}else{return 1;}
 	if (sysctlbyname("kern.hostname", NULL, &len_hostname, NULL, 0) == 0){
-
-                if(sysctlbyname("kern.hostname", hostname, &len_hostname, NULL, 0) == -1){
-                        perror("sysctl kern.hostname");
-                        return 1;
-                }
+                
+                hostname_ptr = malloc(len_hostname);
+                if (hostname_ptr != NULL){
+                        if (sysctlbyname("kern.hostname", hostname_ptr, &len_hostname, NULL, 0) == -1){
+                                perror("sysctl kern.hostname");
+                                return 1;
+                        }
+                }else{return 1;}
+                
         }else{return 1;}
-	printf("\tOS: %s\n", osversion);
-	printf("\tHostname: %s\n", hostname);
+	printf("\tOS: %s\n", osversion_ptr);
+	printf("\tHostname: %s\n", hostname_ptr);
+	free(osversion_ptr);
+	free(hostname_ptr);
 	return 0;
 
 }
